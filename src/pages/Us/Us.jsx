85d9fbb014f6acf340bbe72ce3d9791eca757dd7
Alexis Franco <108986498+Alexis196@users.noreply.github.com>
2023-12-06 18:59:48 -0300

    pages services, mas modificaciones

---
+++ src/pages/Us/Us.jsx
@@ -0,0 +1,13 @@
+import BannerUs from "../../components/BanerUs/BannerUs"
+import NosotrosPage from "../../components/InfoUs/InfoUs"
+
+const Us = () => {
+  return (
+    <div>
+        <BannerUs />
+        <NosotrosPage />
+    </div>
+  )
+}
+
+export default Us
